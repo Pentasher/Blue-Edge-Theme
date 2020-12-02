32725afc6f78077aeee38aa0639eeeadffb132a2
Pentasher <pentasher@gmail.com>
2020-12-02 21:26:30 +0800

    File uploaded

---
+++ wp-content/themes/blueedgetheme/front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">Test</div>
+                <div class="col">Test</div>
+            </div>
+        </div>
+    </section>
+<?php get_footer(); ?>
---
+++ wp-content/themes/blueedgetheme/functions.php
@@ -0,0 +1,3 @@
+<?php 
+
+print( get_template_directory_uri() . '/css/bootstrap.min.css' );
